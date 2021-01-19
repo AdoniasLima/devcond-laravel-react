d38e29cad57e41a851ef2caa6485c3844d5f5f8c
AdoniasLima <adoniaslimagouveia@outlook.com>
2021-01-19 15:50:07 -0300

    [Feature] - Backend - Creating seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +13,59 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    {   
+        //Units
+        DB::table("units")->insert([
+            "name" => "APT 100",
+            "id_owner" => 1
+        ]);
+        DB::table("units")->insert([
+            "name" => "APT 101",
+            "id_owner" => 1
+        ]);
+        DB::table("units")->insert([
+            "name" => "APT 200",
+            "id_owner" => 0
+        ]);
+        DB::table("units")->insert([
+            "name" => "APT 201",
+            "id_owner" => 0
+        ]);
+        //Areas
+        DB::table("areas")->insert([
+            "allowed" => 1,
+            "title" => "Academy",
+            "cover" => "gym.jpg",
+            "days" => "1,2,4,5",
+            "start_time" => "06:00:00",
+            "end_time" => "22:00:00"
+        ]);
+        DB::table("areas")->insert([
+            "allowed" => 1,
+            "title" => "Pool",
+            "cover" => "pool.jpg",
+            "days" => "1,2,3,4,5",
+            "start_time" => "07:00:00",
+            "end_time" => "23:00:00"
+        ]);
+        DB::table("areas")->insert([
+            "allowed" => 1,
+            "title" => "Barbecue grill",
+            "cover" => "barbecue.jpg",
+            "days" => "4,5,6",
+            "start_time" => "09:00:00",
+            "end_time" => "23:00:00"
+        ]);
+        //Walls
+        DB::table("walls")->insert([
+            "title" => "Hello",
+            "body" => "All ok",
+            "datecreated" => "2020-01-19 15:00:00"
+        ]);
+        DB::table("walls")->insert([
+            "title" => "Alert",
+            "body" => "Bla bla bla",
+            "datecreated" => "2020-01-19 19:00:00"
+        ]);
     }
 }
